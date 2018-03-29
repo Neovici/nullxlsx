d464300166d2d3e6ddb3bac56e5f492b914c388d
Andrei Alexandru <alexconstantin.alexandru@gmail.com>
2018-03-29 16:30:12 +0300

    const instead of var

--- experiments/test-require.js
+++ experiments/test-require.js
@@ -1,12 +1,12 @@
 
-var nz = require('./index.js');
+const nz = require('./index.js'),
+	zip = new nz.NullZipArchive('zipfile.zip');
 
 console.info(nz);
 console.info(nz.NullZipArchive);
-
-var zip = new nz.NullZipArchive('zipfile.zip');
 console.info(zip);
+
 zip.addFileFromString('text.txt', 'This is content');
-var buffer = zip.generate();
+const buffer = zip.generate();
 
 console.info('zip file ' + buffer.length);
